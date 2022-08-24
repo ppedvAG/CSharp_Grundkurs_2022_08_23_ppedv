c4296acb87dd5efcfc9eb10dd7a449e874d8d04d
ppedvLukasK <lukask@ppedv.de>
2022-08-24 12:04:14 +0200

    M006

---
+++ M006/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace M006;
+
+class Person
+{
+	#region Variable
+	private string nachname;
+
+	public string GetName()
+	{
+		return nachname;
+	}
+
+	public void SetName(string neuerName)
+	{
+		if (neuerName.Length >= 2 && neuerName.Length <= 20)
+			nachname = neuerName;
+	}
+	#endregion
+
+	#region Properties
+	private string vorname;
+
+	public string Vorname //Property statt Get/Set Methoden
+	{
+		get => vorname; //Expression Body mit => statt { } und return bei Einzeilern
+		set
+		{
+			//value: string neuerName von oben
+			if (value.Length >= 2 && value.Length <= 20) //Checks auch wieder einbauen
+				vorname = value;
+		}
+	}
+
+	private int gehalt;
+
+	public int Gehalt
+	{
+		get => gehalt;
+		private set => gehalt = value; //Gehalt kann von außen nicht gesetzt werden
+	}
+
+	public int Jahresgehalt //Get-Only Property
+	{
+		get => gehalt * 14;
+	}
+
+	//public int Jahresgehalt => gehalt * 14; Kurzform von Get-Only Property
+
+	public string Lieblingsfarbe { get; set; } = "Blau"; //Standardwert setzen
+	#endregion
+
+	#region Konstruktor
+	//Konstruktor: Entwickler dazu zwingen bestimmte Standardwerte zu übergeben
+	public Person(string vorname, string nachname) //Standardkonstruktor wird überschrieben
+	{
+		this.vorname = vorname; //mit this nach oben greifen
+		this.nachname = nachname;
+	}
+
+	public Person(string vorname, string nachname, int gehalt) : this(vorname, nachname) //Konstruktoren verketten
+	{
+		this.gehalt = gehalt;
+	}
+
+	public Person() => Console.WriteLine("Test"); //Expression Body auch hier möglich (Einzeiler)
+	#endregion
+
+	public void PrintPerson() => Console.WriteLine($"{Vorname} {GetName()}"); //Expression Body auch hier möglich (Einzeiler)
+}
---
+++ M006/Program.cs
@@ -0,0 +1,62 @@
+﻿using M006.NamespaceAusserhalb; //für AndererNamespace
+using System; //enthält Console
+using System.Windows.Input;
+
+namespace M006
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Namespaces
+			Console.WriteLine("Hello, World!");
+			//Console-Klasse: System
+			//Path-Klasse: System.IO
+			//Program-Klasse: M006
+
+			Program p; //M006.Program
+			NamespaceUnterhalb.Program p2; //Anderes Program explizit angeben
+			NamespaceAusserhalb.Program1 p3; //Hier auch explizit angeben
+			AndererNamespace.Program p4;
+
+			ICommand command; //using System.Windows.Input erforderlich
+			#endregion
+
+			Person person = new Person(); //Neues Objekt aus dem Bauplan erstellen
+			person.SetName("Max"); //Nachname setzen (mit Überprüfung)
+			Console.WriteLine(person.GetName()); //Nachname auslesen
+
+			person.Vorname = "Max"; //Properties kann man wie eine ganz normale Variable benutzen
+			Console.WriteLine(person.Vorname);
+
+			//person.Gehalt = 1000; funktioniert nicht
+			if (person.Gehalt == 1000) { } //da get nicht private ist das hier möglich
+
+			//person.Jahresgehalt = 10000; funktioniert nicht
+			Console.WriteLine(person.Jahresgehalt);
+
+			Person person1 = new Person("Max", "Mustermann"); //Konstruktor mit Parametern aufrufen
+
+			Person person2 = new Person("Max", "Mustermann", 2000); //Verkettete Konstruktoren
+			person2.PrintPerson(); //Funktion aus Person benutzen
+		}
+	}
+
+	namespace NamespaceUnterhalb //M006.NamespaceUnterhalb
+	{
+		class Program { } //Weitere Program Klasse möglich
+	}
+}
+
+namespace M006.NamespaceAusserhalb
+{
+	class Program1 { }
+}
+
+namespace AndererNamespace
+{
+	class Program 
+	{
+		Program1 p; //Hier using M006.NamespaceAusserhalb erforderlich
+	}
+}
