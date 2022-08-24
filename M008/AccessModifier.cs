1f64ed00b997b572ee38ae5cd43c17c7e3334b36
ppedvLukasK <lukask@ppedv.de>
2022-08-24 16:42:41 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,17 @@
+﻿namespace M008;
+
+public class AccessModifier //Klassen/Member ohne Modifier sind internal
+{
+	public string Name { get; set; } //Kann überall gesehen werden (auch außerhalb vom Projekt)
+
+	private int Groesse { get; set; } //Kann nur in dieser Klasse gesehen werden
+
+	internal string Wohnort { get; set; } //Kann überall aber nur in diesem Projekt gesehen werden
+
+
+	protected string Lieblingsfarbe { get; set; } //Kann nur in dieser Klasse und in Unterklassen gesehen werden, auch außerhalb vom Projekt (effektiv private aber funktioniert auch in Vererbungshierarchien)
+
+	protected internal string Lieblingsnahrung { get; set; } //Kann im Projekt überall gesehen werden und in Unterklassen außerhalb vom Projekt
+
+	protected private DateTime Geburtsdatum { get; set; } //Kann nur in dieser Klasse und Unterklasse nur im Projekt gesehen werden
+}
---
+++ M008/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace M008;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Max", 32);
+		m.WasBinIch(); //Nicht im Mensch definiert aber trotzdem möglich
+	}
+}
+
+public class Lebewesen //Basisklasse
+{
+	public string Name { get; set; }
+
+	public Lebewesen(string name) => Name = name;
+
+	public virtual void WasBinIch() //Wird auch nach unten vererbt
+	{
+		Console.WriteLine("Ich bin ein Lebewesen");
+	}
+}
+
+public sealed class Mensch : Lebewesen //Mensch ist ein Lebewesen (Vererbung herstellen)
+{
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name) //Konstruktoren verketten mit base(...)
+	{
+		Alter = alter;
+	}
+
+	//public void WasBinIch() => Console.WriteLine($"Ich bin ein Mensch und bin {Alter} Jahre alt"); //Keine Überschreibung sondern nur versteckt
+
+	public sealed override void WasBinIch() //override: Überschreibe die Methode von oben, obere muss Methode muss virtual sein
+	{
+		//base.WasBinIch(); //base: nach oben greifen in der Vererbungshierarchie
+		Console.WriteLine($"Ich bin ein Mensch, mein Name ist {Name} und bin {Alter} Jahre alt"); //Name wird vererbt
+	}
+}
+
+//public class Kind : Mensch //Vererbung nicht möglich weil Mensch sealed ist
+//{
+//	public Kind(string name, int alter) : base(name, alter) { }
+//
+//	public override void WasBinIch() { } nicht möglich, weil sealed
+//}
